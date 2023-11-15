ddabaa30296f66be45c85433b116618f17292694
sahith <honey.sahith29@gmail.com>
2023-11-15 15:57:33 +0530

    added tailwind , chakra ui , font awesome and created contact and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import Contact from "./components/Contact"
 import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+
+      </Routes>
+
+
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
 )
