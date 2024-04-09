2c6d5fd0f9e4973f0cca6ca3a70c8b4a8d12c353
sahith <honey.sahith29@gmail.com>
2024-04-09 08:52:20 +0530

    added bgmi

--- src/components/pages/Homepage.tsx
+++ src/components/pages/Homepage.tsx
@@ -1,5 +1,6 @@
 import Dropdown from '../justComponents/Dropdown';
 import student from "../../assets/student.png";
+import bgmiHashOne from "../../assets/bgmiHashOne.png";
 import { TypeAnimation } from 'react-type-animation';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../reduxToolkit/Store';
@@ -10,36 +11,44 @@ function Homepage() {
     const dispatch = useDispatch();
     console.log(count);
     return (
-        <div className="items-center mb-5 sm:min-h-[100vh] sm:flex justify-around text-center">
-            <div className="flex flex-col items-center mb-6">
+        <>
+            <div className="items-center mb-5 sm:min-h-[100vh] sm:flex justify-around text-center">
+                <div className="flex flex-col items-center mb-6">
+                    <img
+                        className="sm:h-[27rem] h-[10rem] rounded-[0rem] mb-5"
+                        src={student}
+                        alt="Profile"
+                    />
+                    <h1 onClick={() => dispatch(increment())} className="select-none text-3xl font-bold text-gray-800 mt-4">
+                        <TypeAnimation
+                            sequence={[
+                                "Sahith...",
+                                1000,
+                                "Student",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={8}
+                            className="text-purple-600 font-amita"
+                            repeat={Infinity}
+                        />
+                    </h1>
+                </div>
+                <div className="sm:text-left">
+                    <div className="text-[3rem] font-amita">Hey,</div>
+                    <div className="text-[3rem] font-amita">This is <span className="text-orange-500 ">Sahith</span></div>
+                    <div className="text-[3rem] font-amita">Into Tech? </div>
+                    <Dropdown />
+                </div>
+            </div>
+            <div className='flex justify-center'>
                 <img
-                    className="sm:h-[27rem] h-[10rem] rounded-[0rem] mb-5"
-                    src={student}
+                    className="sm:h-[23rem] rounded-[0rem] mb-10"
+                    src={bgmiHashOne}
                     alt="Profile"
                 />
-                <h1 onClick={() => dispatch(increment())} className="select-none text-3xl font-bold text-gray-800 mt-4">
-                    <TypeAnimation
-                        sequence={[
-                            "Sahith...",
-                            1000,
-                            "Student",
-                            1000,
-                        ]}
-                        wrapper="span"
-                        speed={8}
-                        className="text-purple-600 font-amita"
-                        repeat={Infinity}
-                    />
-                </h1>
             </div>
-            <div className="sm:text-left">
-                <div className="text-[3rem] font-amita">Hey,</div>
-                <div className="text-[3rem] font-amita">This is <span className="text-orange-500 ">Sahith</span></div>
-                <div className="text-[3rem] font-amita">Into Tech? </div>
-                <Dropdown />
-            </div>
-
-        </div>
+        </>
     );
 }
 
